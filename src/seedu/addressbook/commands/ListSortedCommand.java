2aacb296d06d3bd44d9cf963a97ac92ad13dda12
unusep <unusep@gmail.com>
2016-09-26 21:59:30 +0800

    Implement ListSorted sorted by name

--- src/seedu/addressbook/commands/ListSortedCommand.java
+++ src/seedu/addressbook/commands/ListSortedCommand.java
@@ -2,6 +2,7 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -16,10 +17,19 @@ public class ListSortedCommand extends Command {
             + "Displays all persons in the address book as a list with index numbers sorted by name.\n\t"
             + "Example: " + COMMAND_WORD;
 
-
+    
+    class NameComparator implements Comparator<ReadOnlyPerson> {
+    	@Override
+    	public int compare(ReadOnlyPerson a, ReadOnlyPerson b){
+    		String aName = a.getName().toString();
+    		String bName = b.getName().toString();
+    		return aName.compareTo(bName);
+    	}
+    }
     @Override
     public CommandResult execute() {
         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        allPersons.sort(new NameComparator());
         return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
     }
 }
