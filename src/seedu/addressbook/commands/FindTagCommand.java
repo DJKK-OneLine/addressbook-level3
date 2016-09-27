2353487dd2aed62a33d4960ba22055072e443f20
ongks <ongkashing@gmail.com>
2016-09-27 23:11:22 +0800

    added feature find by tag

---
+++ src/seedu/addressbook/commands/FindTagCommand.java
@@ -0,0 +1,60 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.tag.Tag;
+
+import java.util.*;
+
+/**
+ * Finds and lists all persons in address book whose tags contains any of the argument keywords.
+ * Keyword matching is case sensitive.
+ */
+public class FindTagCommand extends Command {
+
+    public static final String COMMAND_WORD = "findtag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Finds all persons whose tags contain any of "
+            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n\t"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n\t"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final Set<String> keywords;
+
+    public FindTagCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * Returns copy of keywords in this command.
+     */
+    public Set<String> getKeywords() {
+        return new HashSet<>(keywords);
+    }
+
+    @Override
+    public CommandResult execute() {
+        final List<ReadOnlyPerson> personsFound = getPersonsWithTagsContainingAnyKeyword(keywords);
+        return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
+    }
+
+    /**
+     * Retrieve all persons in the address book whose tags contain some of the specified keywords.
+     *
+     * @param keywords for searching
+     * @return list of persons found
+     */
+    private List<ReadOnlyPerson> getPersonsWithTagsContainingAnyKeyword(Set<String> keywords) {
+        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : addressBook.getAllPersons()) {
+            final Set<String> tagsInName = new HashSet<>();
+            for(Tag tag : person.getTags()) {
+            	tagsInName.add(tag.tagName);
+            }
+            if (!Collections.disjoint(tagsInName, keywords)) {
+                matchedPersons.add(person);
+            }
+        }
+        return matchedPersons;
+    }
+
+}
